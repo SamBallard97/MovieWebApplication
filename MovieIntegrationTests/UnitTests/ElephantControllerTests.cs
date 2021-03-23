8c89a1f2ca55d3d881c47528a7b3cbb491df70a2
Sam <sam.ballard1997@googlemail.com>
2021-03-23 09:00:30 +0000

    Testing for getelephant

--- MovieIntegrationTests/UnitTests/ElephantControllerTests.cs
+++ MovieIntegrationTests/UnitTests/ElephantControllerTests.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using MovieWebApplication.Controllers;
 using MovieWebApplication.Models;
 using MovieWebApplication.Services;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using Xunit;
 
 namespace MovieWebApplicationTests.UnitTests
@@ -85,10 +87,67 @@ namespace MovieWebApplicationTests.UnitTests
             var result = (OkObjectResult)response.Result;
 
             //assert
+            Assert.NotNull(result.Value);
             Assert.Equal(correctElephant, result.Value);
+            Assert.True(result is OkObjectResult);
+            Assert.IsType<Elephant>(result.Value);
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
 
-            
+        [Fact]
+        public void GetElephant_ReturnsNull()
+        {
+            //arrange
+            var searchedGuid = Guid.NewGuid();
+            var incorrectElephant1 = new Elephant()
+            {
+                Name = "Barry",
+                Id = Guid.NewGuid(),
+                Note = "This is NOT the elephant we want to get"
+            };
+            var incorrectElephant2 = new Elephant()
+            {
+                Name = "Steve",
+                Id = Guid.NewGuid(),
+                Note = "This is NOT the elephant we want to get"
+            };
 
+            var elephantList = new List<Elephant>();
+            elephantList.Add(incorrectElephant1);
+            elephantList.Add(incorrectElephant2);
+            var controller = new ElephantController(_elephantService.Object);
+            _elephantService.Setup(x => x.GetElephants(It.IsAny<string>())).Returns(elephantList);
+
+            //act
+            var response = controller.GetElephant(searchedGuid);
+            var result = (NotFoundResult)response.Result;
+
+            //assert
+            Assert.Equal(404, result.StatusCode);
+
+            Assert.NotNull(result);
+            Assert.True(result is NotFoundResult);
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        }
+
+        [Fact]
+        public void GetElephant_OnEmptyList()
+        {
+            //arrange
+            var elephantList = new List<Elephant>();
+            var controller = new ElephantController(_elephantService.Object);
+            _elephantService.Setup(x => x.GetElephants(It.IsAny<string>())).Returns(elephantList);
+
+            //act
+            var response = controller.GetElephant(Guid.NewGuid());
+            var result = (NotFoundResult)response.Result;
+
+            //assert
+            Assert.Equal(404, result.StatusCode);
+
+            Assert.NotNull(result);
+            Assert.True(result is NotFoundResult);
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
         }
     }
 }
